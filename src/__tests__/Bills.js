306ee5d867a03b4e5e1b5bcd9ed3775db7392b7c
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-08-15 01:50:00 +0200

    improve tests Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,39 +1,35 @@
-/**
- * @jest-environment jsdom
- */
+import { screen } from '@testing-library/dom';
+import { bills } from '../fixtures/bills.js';
+import BillsUI from '../views/BillsUI.js';
 
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+describe('Given I am connected as an employee', () => {
+  describe('When I am on Bills Page and there are no bill', () => {
+    test('Then bills should be empty', () => {
+      const html = BillsUI({ data: [] });
+      document.body.innerHTML = html;
+      //to-do write expect expression // DONE
+      const eyeIconElt = screen.queryByTestId('icon-eye');
+      expect(eyeIconElt).toBeNull();
+    });
+  });
 
-import router from "../app/Router.js";
+  describe('When I am on Bills Page and there are bill(s)', () => {
+    test('Then bills should be ordered from earliest to latest', () => {
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      // recupere les dates
+      const dates = Array.from(
+          document.body.querySelectorAll('#data-table tbody>tr>td:nth-child(3)')
+      ).map((a) => a.innerHTML);
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+      console.log(dates);
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      console.log(datesSorted);
 
-    })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      expect(dates).toEqual(datesSorted);
+      // screen.debug();
+    });
+  });
+});
