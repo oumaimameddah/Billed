ee98a7e5f62f898e1a2c1c7cac4c696048246cd8
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-08-15 02:11:24 +0200

    ajouter des tests unitaire NewBills et fiw des erreurs

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,84 +1,99 @@
 import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import { bills } from '../fixtures/bills.js';
+import Bills from '../containers/Bills.js';
 import BillsUI from '../views/BillsUI.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
-import userEvent from '@testing-library/user-event';
+import { ROUTES } from '../constants/routes';
+import firebase from "../__mocks__/firebase";
+
+// UNIT TESTS : CONNECTED AS EMPLOYEE
 
 describe('Given I am connected as an employee', () => {
-  // test : loading page on BillUI
+  // parcours employe
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+  });
+  window.localStorage.setItem(
+      'user',
+      JSON.stringify({
+        type: 'Employee',
+      })
+  );
+
+  // TEST : loading page BillsUI
   test('Then, Loading page should be rendered', () => {
     const html = BillsUI({ loading: true });
     document.body.innerHTML = html;
 
+    // expected result
     expect(screen.getAllByText('Loading...')).toBeTruthy();
   });
 
-  // test : Error on bills page
+  // TEST : Error on BillsUI page
   describe('When I am on Bills page and back-end send an error message', () => {
     test('Then, Error page should be rendered', () => {
       document.body.innerHTML = BillsUI({ error: 'some error message' });
 
+      // expected result
       expect(screen.getAllByText('Erreur')).toBeTruthy();
     });
   });
 
-  // test : vertical icon visible in vertical layout
+  // TEST : bill icon visible in vertical layout
   describe('When I am on Bills page', () => {
     test('Then bill icon in vertical layout should be visible', () => {
-      Object.defineProperty(window, 'localStorage', {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-          'user',
-          JSON.stringify({
-            type: 'Employee',
-          })
-      );
-
+      // DOM construction
       document.body.innerHTML = BillsUI({ data: [] });
 
+      // get icon in DOM
       const billIcon = screen.getByTestId('icon-window');
 
+      // result expected
       expect(billIcon).toBeTruthy();
     });
   });
 
-  // test : empty table if no bill
+  // TEST : empty table if no bill
   describe('When I am on Bills Page and there are no bill', () => {
     test('Then bills should render an empty table', () => {
+      // DOM construction
       document.body.innerHTML = BillsUI({ data: [] });
 
-      const table = screen.queryByTestId('table');
+      // get DOM element
       const eyeIcon = screen.queryByTestId('icon-eye');
 
-      expect(table.innerHTML).toBe('');
+      // expected result
       expect(eyeIcon).toBeNull();
+
+      // get DOM element
+      // const table = screen.queryByTestId('tbody');
+
+      // expected result
+      // expect(table.innerHTML).toBe("");
+
+      // ERREUR --------------------
+      // expect(received).toBe(expected) // Object.is equality
+      // Expected: ""
+      // Received: "·············"
     });
   });
 
-  // test : redirected on newBill page if clic on new bill button
-  describe('When I am on Bills page and I click on the new bill button', () => {
-    test('Then I should be sent to newBill page', () => {
-      Object.defineProperty(window, 'localStorage', {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-          'user',
-          JSON.stringify({
-            type: 'Employee',
-          })
-      );
+  // TEST : redirected on newBill page if click on new bill button
+  describe('When I am on Bills page and I click on the new bill button Nouvelle note de frais', () => {
+    test('Then I should navigate to newBill page bill/new', () => {
+      // DOM construction
+      document.body.innerHTML = BillsUI({ bills });
 
-      const html = BillsUI({ data: bills });
-      document.body.innerHTML = html;
-
-
+      // init onNavigate
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
 
+      // init firestore
       const firestore = null;
 
+      // init bills display
       const billsContainer = new Bills({
         document,
         onNavigate,
@@ -86,38 +101,33 @@ describe('Given I am connected as an employee', () => {
         localStorage: window.localStorage,
       });
 
+      // handle click event
       const handleClickNewBill = jest.fn(billsContainer.handleClickNewBill);
       const newBillButton = screen.getByTestId('btn-new-bill');
       newBillButton.addEventListener('click', handleClickNewBill);
       userEvent.click(newBillButton);
 
+      // expected results
       expect(handleClickNewBill).toHaveBeenCalled();
       expect(screen.getByText('Envoyer une note de frais')).toBeTruthy();
     });
   });
 
+  // TEST : click on icon eye opens modal & display attached image
   describe('When I am on Bills page and I click on an icon eye', () => {
     test('Then a modal should open', () => {
-      Object.defineProperty(window, 'localStorage', {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-          'user',
-          JSON.stringify({
-            type: 'Employee',
-          })
-      );
+      // DOM construction
+      document.body.innerHTML = BillsUI({ data: bills });
 
-      const html = BillsUI({ data: bills });
-      document.body.innerHTML = html;
-
-
+      // init onNavigate
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
 
+      // init firestore
       const firestore = null;
 
+      // init bills display
       const billsContainer = new Bills({
         document,
         onNavigate,
@@ -125,27 +135,68 @@ describe('Given I am connected as an employee', () => {
         localStorage: window.localStorage,
       });
 
-      const iconEye = screen.getByTestId('icon-eye47qAXb6fIm2zOKkLzMro');
+      // get DOM element
+      const iconEye = screen.getAllByTestId('icon-eye')[0];
+
+      // handle click event
       const handleClickIconEye = jest.fn(
           billsContainer.handleClickIconEye(iconEye)
       );
       iconEye.addEventListener('click', handleClickIconEye);
       userEvent.click(iconEye);
 
+      // expected result
       expect(handleClickIconEye).toHaveBeenCalled();
+    });
 
-      const modale = screen.getByTestId('modaleFile');
+    test('Then the modal should display the attached image', () => {
+      // DOM construction
+      document.body.innerHTML = BillsUI({ data: bills });
 
-      expect(modale).toBeTruthy();
+      // init onNavigate
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // init firestore
+      const firestore = null;
+
+      // init bills display
+      const billsContainer = new Bills({
+        document,
+        onNavigate,
+        firestore,
+        localStorage: window.localStorage,
+      });
+
+      // get DOM element
+      const iconEye = screen.getAllByTestId('icon-eye')[0];
+
+      // handle click event
+      billsContainer.handleClickIconEye(iconEye);
+
+      // expected results
+      expect(document.querySelector('.modal')).toBeTruthy();
     });
   });
 
   // test : bills ordered from earliest to latest
+  // ORIGINAL
+  /*test("Then bills should be ordered from earliest to latest", () => {
+    const html = BillsUI({ data: bills })
+    document.body.innerHTML = html
+    const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+    const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+    const datesSorted = [...dates].sort(antiChrono)
+    expect(dates).toEqual(datesSorted)
+  })*/
+
   describe('When I am on Bills Page and there are bill(s)', () => {
     test('Then bills should be ordered from earliest to latest', () => {
-      const html = BillsUI({ data: bills });
-      document.body.innerHTML = html;
+      // DOM element
+      document.body.innerHTML = BillsUI({ data: bills });
 
+      // frecnh months array
       const frenchMonths = [];
       for (let i = 0; i < 12; i++) {
         frenchMonths.push(
@@ -156,7 +207,7 @@ describe('Given I am connected as an employee', () => {
       }
 
       // reverse Us date (year, month, day) into Fr date (day, month, year)
-      const formatDateReverse = (formatedDate) => {
+      const reverseDate = (formatedDate) => {
         let [day, month, year] = formatedDate.split(' ');
         day = parseInt(day);
         month = frenchMonths.findIndex(
@@ -167,17 +218,71 @@ describe('Given I am connected as an employee', () => {
         return new Date(year, month, day);
       };
 
-      document.body.innerHTML = BillsUI({ data: billsSample });
+      document.body.innerHTML = BillsUI({ data: bills });
 
+      // get dates from table
       const dates = Array.from(
           document.body.querySelectorAll('#data-table tbody>tr>td:nth-child(3)')
       ).map((a) => a.innerHTML);
 
+      // sort from earliest to latest
       const antiChronoSort = (a, b) =>
-          formatDateReverse(a) < formatDateReverse(b) ? 1 : -1;
+          reverseDate(a) < reverseDate(b) ? 1 : -1;
       const datesSorted = [...dates].sort(antiChronoSort);
 
+      // expected result
       expect(dates).toEqual(datesSorted);
+    });
+  });
+
+  // INTEGRATION GET TESTS
+
+  describe('Given I am a user connected as Employee', () => {
+    describe('When I navigate to BillsUI', () => {
+      // TEST : bills fetch from API
+      test('fetches bills from mock API GET', async () => {
+        // spy on Firebase Mock
+        const getSpy = jest.spyOn(firebase, 'get');
+
+        // get bills
+        const bills = await firebase.get();
+
+        // expected results
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(bills.data.length).toBe(4);
+      });
+
+      // TEST : bills fetch failure => 404 error
+      test('fetches bills from an API and fails with 404 message error', async () => {
+        // single use for throw error
+        firebase.get.mockImplementationOnce(() =>
+            Promise.reject(new Error('Erreur 404'))
+        );
+
+        // DOM element
+        document.body.innerHTML = BillsUI({ error: 'Erreur 404' });
+
+        // await response
+        const message = await screen.getByText(/Erreur 404/);
+
+        // expected result
+        expect(message).toBeTruthy();
+      });
+
+      // TEST messages fetch failure => 500 error
+      test('fetches messages from an API and fails with 500 message error', async () => {
+        // single use for throw error
+        firebase.get.mockImplementationOnce(() =>
+            Promise.reject(new Error('Erreur 500'))
+        );
+
+        // DOM element
+        document.body.innerHTML = BillsUI({ error: 'Erreur 500' });
+        const message = await screen.getByText(/Erreur 500/);
+
+        // expected result
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,18 +1,161 @@
-/**
- * @jest-environment jsdom
- */
+import { fireEvent, screen } from '@testing-library/dom';
+import BillsUI from '../views/BillsUI.js';
+import NewBillUI from '../views/NewBillUI.js';
+import NewBill from '../containers/NewBill.js';
+import '@testing-library/jest-dom';
+import { localStorageMock } from '../__mocks__/localStorage';
+import firestore from '../app/Firestore.js';
+import { ROUTES } from '../constants/routes';
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+describe('Given I am connected as an employee', () => {
+  // parcours employe
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+  });
 
+  window.localStorage.setItem(
+      'user',
+      JSON.stringify({
+        type: 'Employee',
+      })
+  );
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+  describe('When I am on NewBill Page', () => {
+    // TEST : display newBill page
+    test('Then the newBill page should be rendered', () => {
+      // DOM construction
+      document.body.innerHTML = NewBillUI();
+
+      // expected result
+      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy();
+    });
+
+    // TEST : display 9 fields form
+    test('Then a form with nine fields should be rendered', () => {
+      // DOM construction
+      document.body.innerHTML = NewBillUI();
+
+      // get DOM element
+      const form = document.querySelector('form');
+
+      // expected result
+      expect(form.length).toEqual(9);
+    });
+  });
+
+  describe('When I am on NewBill Page and I add an attached file', () => {
+    // TEST : handle attached file format
+    test('Then the file handler should be run', () => {
+      // DOM construction
+      document.body.innerHTML = NewBillUI();
+
+      // init onNavigate
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // get DOM element
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: firestore,
+        localStorage: window.localStorage,
+      });
+
+      // handle event
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile);
+      const attachedFile = screen.getByTestId('file');
+      attachedFile.addEventListener('change', handleChangeFile);
+      fireEvent.change(attachedFile, {
+        target: {
+          files: [new File(['file.txt'], 'file.txt', { type: 'text/txt' })],
+        },
+      });
+
+      // expected result
+      const numberOfFile = screen.getByTestId('file').files.length;
+      expect(numberOfFile).toEqual(1);
+    });
+  });
+
+  describe('WHEN I am on NewBill page and I submit a correct form', () => {
+    // TEST : submit correct form and attached file
+    test('THEN I should be redirected to Bills page', () => {
+      // init onNavigate
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // DOM construction
+      document.body.innerHTML = NewBillUI();
+
+      // get DOM element
+      const newBillContainer = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      });
+
+      // handle event submit attached file
+      const handleSubmit = jest.fn(newBillContainer.handleSubmit);
+      newBillContainer.fileName = 'image.jpg';
+
+      // handle event submit form
+      const newBillForm = screen.getByTestId('form-new-bill');
+      newBillForm.addEventListener('submit', handleSubmit);
+      fireEvent.submit(newBillForm);
+
+      // expected results
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
+    });
+  });
+
+  describe('When I am on NewBill page and I submit a wrong attached file format', () => {
+    // TEST : wrong attached file format
+    test('Then the error message should be displayed', () => {});
+
+    // init onNavigate
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+
+    // DOM construction
+    document.body.innerHTML = NewBillUI();
+
+    // get DOM element
+    const newBillContainer = new NewBill({
+      document,
+      onNavigate,
+      firestore: null,
+      localStorage: window.localStorage,
+    });
+
+    const handleChangeFile = jest.fn(newBillContainer.handleChangeFile);
+
+    const attachedFile = screen.getByTestId('file');
+    attachedFile.addEventListener('change', handleChangeFile);
+    fireEvent.change(attachedFile, {
+      target: {
+        files: [
+          new File(['document.pdf'], 'document.pdf', {
+            type: 'application/pdf',
+          }),
+        ],
+      },
+    });
+
+    // expected results
+    expect(handleChangeFile).toHaveBeenCalled();
+    expect(attachedFile.files[0].name).toBe('document.pdf');
+
+    // get DOM element
+    const errorMessage = screen.getByTestId('fileFormat-errorMessage');
+
+    // expected results
+    expect(errorMessage.textContent).toEqual(
+        expect.stringContaining('Votre justificatif doit être une image de format (.jpg) ou (.jpeg) ou (.png)')
+    );
+  });
+});
