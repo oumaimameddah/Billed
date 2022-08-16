b32b0843bc76aaf0ceb47ec4183e8d87b0f0b060
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-08-16 02:17:38 +0200

    fix ([Bug report] - Bills) trier les elèments de dashboard en ordre décroissante de date et fixer les tests unitaire

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,11 +20,12 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+    const dataSorted = data && data.length ? data.sort((a, b) => new Date(b.date) - new Date(a.date)) : "";
+  return (data && data.length) ? dataSorted.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +48,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
